6952930fdf269d3c67b4e65914177821bce21b95
S. Modha Madhan <95305983+modhamadhan@users.noreply.github.com>
2023-02-01 16:31:45 +0530

    commit

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,4 +4,6 @@ from mainapp import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
   ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'page/index.html')
+
+
+def about(request):
+    return render(request, 'page/about.html')
+
+
+def services(request):
+    return render(request, 'page/services.html')
