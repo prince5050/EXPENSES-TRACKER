2e2a9cc52192fc7d3a9792f120fb5eca65d7adff
Thrishanimmalapudi <trishanimmalapudi2001@gmail.com>
2023-02-01 16:37:54 +0530

    commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+
+
 # Create your views here.
 def home(request):
     return render(request, 'page/index.html')
-
+def contact(request):
+    return render(request, 'page/contact.html')
