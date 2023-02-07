2276e8a1ed207ce1fc5b21f0d65f36b0734b9e53
S. Modha Madhan <95305983+modhamadhan@users.noreply.github.com>
2023-02-07 14:24:00 +0530

    commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -23,7 +25,32 @@ def login(request):
     return render(request, 'page/login.html')
 
 def register(request):
-    return render(request, 'page/register.html')
+    if request.method == 'POST':
+    #     first_name = request.POST.get('fname')
+    #     last_name = request.POST.get('lname')
+    #     email = request.POST.get('email')
+    #     password = request.POST.get('password')
+        if User.objects.filter(email=request.POST.get('email')).first() :
+            messages.error(request, 'Email is already taken. Try with another')
+            return render(request, 'page/register.html')
+        else:
+            user_obj = User.objects.create_user(username=request.POST.get('email'), first_name=request.POST.get('first_name'), last_name=request.POST.get('last_name'),
+                                                email=request.POST.get('email'), password=request.POST.get('password'))
+            user_obj.save()
+            messages.info(request, 'Registration has successfully completed')
+            return render(request, 'page/register.html')
+
+    else:
+        return render(request, 'page/register.html')
+
+
+
 
 def forgot(request):
     return render(request, 'page/forgot.html')
+
+
+
+
+
+
