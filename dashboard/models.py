2ac986a6a3168b5d44742a5ad59ba3ccbf8eeb53
S. Modha Madhan <95305983+modhamadhan@users.noreply.github.com>
2023-02-18 10:21:24 +0530

    commit

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+from django.contrib.auth.models import User
+
+SELECT_CATEGORY_CHOICES = [
+    ("Food", "Food"),
+    ("Travel", "Travel"),
+    ("Shopping", "Shopping"),
+    ("Necessities", "Necessities"),
+    ("Entertainment", "Entertainment"),
+    ("Other", "Other")
+]
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    expense_name = models.CharField(max_length=120, unique=True)
+    Category = models.CharField(max_length=20, choices=SELECT_CATEGORY_CHOICES, default='Food')
+    # expense_type = models.CharField(max_length=120)
+    amount = models.CharField(max_length=120)
+    mode_of_payment = models.CharField(max_length=120)
+    date_of_expense = models.DateTimeField(auto_now_add=False)
+    month_of_expense = models.CharField(max_length=120,default=False)
+    year_of_expense = models.CharField(max_length=120,default=False)
+    def __str__(self):
+        return self.expense_name
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import request, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.template import context
+
+from dashboard.models import Expense
+from expensestracker import settings
+
 
 # Create your views here.
+@login_required()
 def dashboard(request):
-    return render(request, 'dashboard_page/index.html')
-
-def buy_ico(request):
-    print('hi')
-    return render(request, 'dashboard_page/buy-ico.html')
+    obj = Expense.objects.filter(user_id=request.user)
+    amt = []
+    expenses_type = []
+    for i in obj:
+        amt.append(i.amount)
+        expenses_type.append(i.Category)
+    return render(request, 'dashboard_page/index.html', {'amt': amt, 'expenses_type': expenses_type})
+#
+# def buy_ico(request):
+#     print('hi')
+#     return render(request, 'dashboard_page/buy-ico.html')
 
 def manage_expenses(request):
     return render(request, 'dashboard_page/manage expenses.html')
@@ -35,9 +51,46 @@ def add_expense(request):
 
 def view_expense(request):
     return render(request, 'dashboard_page/view expense.html')
+
 def edit_expense(request):
     return render(request, 'dashboard_page/edit expense.html')
 
+
+
+# def add_expense(request):
+#     if request.method == 'POST':
+#         if User.objects.filter(Expense_name=request.POST.get('expense_name')).first():
+#             messages.error(request, 'Expense name is already exists')
+#             return render(request, 'dashboard_page/add expense.html')
+#         else:
+#             user_obj = User.objects.add_expense(Expense_name=request.POST.get('expense name'),
+#                                                 Category=request.POST.get('category'),
+#                                                 Expense_type=request.POST.get('expense type'),
+#                                                 amount=request.POST.get('amount'),
+#                                                 date_of_expense=request.POST.get('date of expense'), mode_of_payment=request.POST.get('mode of payment'))
+#             user_obj.save()
+#             messages.success(request, 'Expense added successfully')
+#             return render(request, 'dashboard_page/add expense.html')
+#     else:
+#         return render(request,'dashboard_page/add expense.html')
+
+
+        # try:
+        #     expense_name = request.post['expense_name']
+        #     if not expense_name:
+        #         messages.error(request,'Expense name is required')
+        #         return render(request, 'dashboard_page/add_expense.html',context)
+        #     Category = request.POST['category']
+         #    expense_type = request.post['expense_type']
+        #     if not expense_type:
+        #         messages.error(request,'Expense type is required')
+        #         return render(request, 'dashboard_page/add_expense.html',context)
+
+        #
+        #
+        #
+        # except:
+        #     return redirect('dashboard_page/manage expenses.html')
 
 
 
